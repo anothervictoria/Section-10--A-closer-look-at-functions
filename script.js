07fd5c946d7981befadcc4c22374f51643daacf2
anothervictoria <victoria.panidi@gmail.com>
2024-05-17 13:37:05 +0400

    Functions Accepting Callback Functions

--- script.js
+++ script.js
@@ -1,62 +1,92 @@
 'use strict';
 
-// Default parameters
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // ES5 way of doing
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
+// // Default parameters
+// const bookings = [];
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5 way of doing
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking('LH123');
-createBooking('BGJ345', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+// createBooking('LH123');
+// createBooking('BGJ345', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
 
-createBooking('LH123', undefined, 10); // if we want to skip the second parameter
+// createBooking('LH123', undefined, 10); // if we want to skip the second parameter
 
-// 130. How Passing Arguments Works: Value vs. Reference
+// // 130. How Passing Arguments Works: Value vs. Reference
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 2347367346565,
-};
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 2347367346565,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr.' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr.' + passenger.name;
 
-  if (passenger.passport === 2347367346565) {
-    alert('Check in');
-  } else {
-    alert('Wrong passport number!');
-  }
-};
+//   if (passenger.passport === 2347367346565) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport number!');
+//   }
+// };
 
+// // checkIn(flight, jonas);
+// // console.log(flight, jonas);
+
+// // // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas; // we copied the reference to the object
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000000);
+//   console.log(person.passport);
+// };
+
+// newPassport(jonas);
 // checkIn(flight, jonas);
-// console.log(flight, jonas);
 
-// // Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas; // we copied the reference to the object
+// 132. Functions Accepting Callback Functions
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000000000);
-  console.log(person.passport);
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
-newPassport(jonas);
-checkIn(flight, jonas);
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformer string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
