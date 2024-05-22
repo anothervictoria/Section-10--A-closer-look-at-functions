8bba6378f39837135f14000f7f75b3bc392ece64
anothervictoria <victoria.panidi@gmail.com>
2024-05-22 15:23:43 +0400

    closure challenges and examples

--- script.js
+++ script.js
@@ -308,3 +308,59 @@ booker();
 booker();
 
 console.dir(booker);
+
+//////////////////////////////////////////////////////////////////
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There arer 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// setTimeout(function () {
+//   console.log(`TIMER`);
+// }, 1000);
+const perGroup = 1034;
+boardPassengers(180, 3);
+
+/////////////////////////////////////
+//Challenge #2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
