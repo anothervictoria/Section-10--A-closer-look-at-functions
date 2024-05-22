6c277302b8c7d8b8f93cd95d63d6d5caf634356d
anothervictoria <victoria.panidi@gmail.com>
2024-05-22 14:30:01 +0400

    closure

--- script.js
+++ script.js
@@ -290,3 +290,21 @@ const runOnce = function () {
 }
 // console.log(isPrivate);
 console.log(notPrivate);
+
+/////////////////////////////////  Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+console.dir(booker);
